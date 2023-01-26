377737c983eb933c34c090a711220de8c1fd2725
Jerry Hogsett <jhogsett@gmail.com>
2023-01-26 11:58:26 -0800

    new scripts

---
+++ COMBVTS.bat
@@ -0,0 +1,1 @@
+ffmpeg -y -i "concat:VTS_01_1.VOB|VTS_01_2.VOB|VTS_01_3.VOB|VTS_01_4.VOB|VTS_01_5.VOB" -c:v copy -strict -2 dvd.mpeg
---
+++ COMBVTS2.bat
@@ -0,0 +1,4 @@
+ffmpeg -y -f concat -i files.txt -c:v copy -strict -2 dvd.mpeg
+
+
+
